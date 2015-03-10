c992e15da5a6ba8ebd05b620bcf8170a7a29d7b8
loic delaveau <ldelaveau@localhost.localdomain>
2015-03-10 18:26:27 +0100

    Add main in gitignore, Add main.c, move list.c to list.c.old

---
+++ main.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+name            : main.c
+date            : 10 mars 2015 
+created by      : ldelaveau
+last modified   : 10 mars 2015 18:23:02
+last changes by : ldelaveau
+purpose         :
+******************************************************************************/
+#include "my_stdio.h"
+
+int main(void){
+  my_printf("Hello World\n");
+  return 0;
+}
